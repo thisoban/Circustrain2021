2bce3226660d01f967ed9e6ce56ac6ca56189e50
Thisoban Mahalingam <thisoban_1995@hotmail.com>
2021-05-17 09:11:42 +0200

    tweaked coding

--- Circustrain2021-test/Train.cs
+++ Circustrain2021-test/Train.cs
@@ -17,14 +17,20 @@ namespace Circustrain2021_test
         public void SortAnimals(List<Animal> InputAnimals)
         {
             
-            List<Animal> Carnivores = InputAnimals.Where(x => x.Diet == Diet.Carnivore).OrderByDescending(x => x.Weight).ToList();
+           // List<Animal> Carnivores = InputAnimals.Where(x => x.Diet == Diet.Carnivore).OrderByDescending(x => x.Weight).ToList();
             List<Animal> BiggestCarnivores = InputAnimals.Where(x => x.Diet == Diet.Carnivore).Where(x=>x.Weight == Weight.Large).ToList();
             List<Animal> MediumCarnivores = InputAnimals.Where(x => x.Diet == Diet.Carnivore).Where(x=>x.Weight == Weight.Medium).ToList();
             List<Animal> SmallCarnivores = InputAnimals.Where(x => x.Diet == Diet.Carnivore).Where(x=>x.Weight == Weight.Small).ToList();
             List<Animal> largeherbivore = InputAnimals.Where(x => x.Diet == Diet.Herbivore).Where(x =>x.Weight == Weight.Large).ToList();
             List<Animal> MediummHerbivore = InputAnimals.Where(x => x.Diet == Diet.Herbivore).Where(x => x.Weight == Weight.Medium).ToList();
             List<Animal> SmallHerbivore = InputAnimals.Where(x => x.Diet == Diet.Herbivore).Where(x => x.Weight == Weight.Small).ToList();
-            OrderAnimalsInWagons(Carnivores);
+            OrderAnimalsInWagons(BiggestCarnivores);
+            OrderAnimalsInWagons(SmallCarnivores);
+            OrderAnimalsInWagons(MediummHerbivore);
+            OrderAnimalsInWagons(MediumCarnivores);
+            OrderAnimalsInWagons(largeherbivore);
+            OrderAnimalsInWagons(SmallHerbivore);
+           
             //OrderAnimalsInWagons(Herbvivores);
 
             foreach (Animal animal in MediumCarnivores)
